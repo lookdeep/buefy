934844ae97841941c416e872da3954b1e01a152f
Guram Kajaia <guram.kajaia@gmail.com>
2022-01-27 21:56:06 +0400

    Using deep watcher for checked rows

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -822,8 +822,12 @@ export default {
         * When checkedRows prop change, update internal value without
         * mutating original data.
         */
-        checkedRows(rows) {
-            this.newCheckedRows = [...rows]
+        // Using deep watching because in vue3 array mutations doesn't trigger watchers
+        checkedRows: {
+            handler(rows) {
+                this.newCheckedRows = [...rows]
+            },
+            deep: true
         },
 
         /*
